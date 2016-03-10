874fe73cb1317546f4930c64b3095fb8238c608f
llluiop <290522165@163.com>
2016-03-10 13:43:41 +0800

    function done

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    print_number(5);
+}
+
+fn print_number(x: i32) {
+    println!("x is: {}", x);
+}
+
+fn foo(x: i32) -> i32 {
+    return x + 1;
+}
+
+fn foo2(x: i32) {
+    x + 1;
 }
